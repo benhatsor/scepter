cce5fb556ecde5ab40e46299cc8de4e598d68c49
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-05-26 19:56:39 +0300

    Create iframe.js

---
+++ iframe.js
@@ -0,0 +1,32 @@
+const iframe = document.querySelector('.html');
+
+// Update iframe
+async function renderFrame() {
+  
+  const resp = await axios.get('https://berryscript.com');
+  
+  iframe.contentDocument.querySelector('html').innerHTML = resp;
+  
+});
+
+renderFrame();
+
+var axios = {
+  'get': (url, options) => {
+    return new Promise((resolve, reject) => {
+      try {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            resolve(JSON.parse(this.responseText));
+          }
+        };
+
+        cors = options ? 'https://berrycors.herokuapp.com/' : '';
+
+        xmlhttp.open('GET', (cors + url), true);
+        xmlhttp.send();
+      } catch(e) { reject(e) }
+    });
+  }
+};
