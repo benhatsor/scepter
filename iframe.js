4f34e539d81b710066e591fb2b71507b4fe1e2bd
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-06-01 12:04:58 +0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -73,8 +73,8 @@ var axios = {
 };
 
 var scepterHTML = `
-    <div class="overlay"></div>
-    <div class="inspector">
+    <div class="overlay scepterElem"></div>
+    <div class="inspector scepterElem">
       <div class="option class">
         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 4h5v8l-2.5-1.5L6 12V4z" fill="currentColor"></path></svg>
         <a>Classes and IDs</a>
@@ -84,8 +84,8 @@ var scepterHTML = `
         <a>Add new CSS</a>
       </div>
     </div>
-    <div class="expanded--overlay"></div>
-    <div class="popover">
+    <div class="expanded--overlay scepterElem"></div>
+    <div class="popover scepterElem">
       <div class="header">
         <div class="type">&lt;div&gt;</div>
         <svg class="close" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z" fill="currentColor"></path></svg>
