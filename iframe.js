8b972cad07d77eff5ada01af7d6086d70578ff3c
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-06-01 12:36:50 +0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -127,17 +127,21 @@ function fireEvent(element, event) {
   evt.initEvent(event, true, true); // event type,bubbling,cancelable
   return !element.dispatchEvent(evt);
 }
-
 var links = document.getElementsByTagName("link");
 var st = [];
 for (var x = 0; x < links.length; x++)
-  if (links[x].getAttribute("rel") == "stylesheet")
+  if (links[x].getAttribute("rel") == "stylesheet") {
     st.push(links[x]);
     links[x].wasAtt = links[x].getAttribute("href");
     links[x].setAttribute("href", "");
+  }
+setTimeout(function() {
   for (var x = 0; x < st.length; x++)
     st[x].setAttribute("href", st[x].wasAtt);
+  setTimeout(function() {
     fireEvent(window, "load");
+  }, 1000);
+}, 1000);
 `;
 
 renderFrame('https://berryscript.com');
