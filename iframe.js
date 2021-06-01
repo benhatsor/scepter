ee8ddb4094b47975d7cb3db16c5ab56a90f392bf
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-06-01 09:50:53 +0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -20,7 +20,7 @@ var axios = {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function () {
           if (this.readyState == 4 && this.status == 200) {
-            resolve(JSON.parse(this.responseText));
+            resolve(this.responseText);
           }
         };
 
