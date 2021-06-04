a9710fa8ba436d443cc9d5cd28ef53da6c78d5da
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-06-04 15:11:57 +0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -142,7 +142,7 @@ async function renderFrame(url) {
   let customElementRegistry = tempFrame.contentWindow.customElements;
   
   customElementRegistry.define('scepter-element', ScepterElement);
-  window.customElements.define('scepter-element', ScepterElement);
+  //window.customElements.define('scepter-element', ScepterElement);
   
   // add the scepter element to dom
   var scepterElem = tempDoc.createElement('scepter-element');
