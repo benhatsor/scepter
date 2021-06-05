c26c70555459f499cc3fe876e38a5a8286c81555
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-06-05 12:09:37 +0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -218,14 +218,15 @@ var scepterHTML = `
 
 var scepterOutlyingCSS = `
 body *:not(scepter-element) {
-  transition: .3s ease;
+  transition: .3s ease transform;
 }
 
-body *:not(scepter-element):active {
+body *:not(scepter-element).acCtive {
   transform: scale(0.94);
+  transition: .3s ease transform !important;
 }
 
-body .seElected *:active {
+body .seElected *.acCtive {
   transform: none;
 }
 
