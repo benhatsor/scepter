d7c2d0fbcd3eaed1f7b85cb69e02dc92a6034c54
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-06-07 14:56:33 +0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -146,6 +146,7 @@ var axios = {
         if (this.readyState == 4 && this.status == 200) {
           resolve(this.responseText);
         } else {
+          console.log(this);
           reject(this.response);
         }
       };
