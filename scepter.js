4857d9300248fd19b28e09a8b5404800c3ab2461
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-19 21:42:42 +0300

    Update scepter.js

--- scepter.js
+++ scepter.js
@@ -358,11 +358,11 @@ var scepter = {
       
     };
 
-    window.onerror = function(message, source) {
+    win.onerror = function(message, source, line) {
       
       console.log('Found error!');
       
-      win.console.logs.push({ content: (message + '\nURL:' + source), type: 'error' });
+      win.console.logs.push({ content: (message + '\nURL: ' + source + '. L: ' + line), type: 'error' });
       
       // if console is open
       if (popoverContent.querySelector('.log')) {
