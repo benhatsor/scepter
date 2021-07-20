51879efeaf5c011c77d93379fea3fa88e1cb4e0a
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-20 11:56:40 +0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -85,7 +85,7 @@ async function renderFrame(url) {
     // get href with base URL
     var newHref = new URL(a.href, url); 
     
-    a.ontouchstart = () => { window.parent.renderFrame(newHref) };
+    a.onclick = () => { window.parent.renderFrame(newHref) };
     
   })
   
