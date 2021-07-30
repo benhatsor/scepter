55c60c47be7b1abf34e316594ce1840fcdaeae25
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-07-30 18:56:33 +0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -151,7 +151,7 @@ async function renderFrame(url) {
     
     // discussion about replacing eval():
     // https://github.com/barhatsor/scepter/issues/2
-    tempFrame.contentWindow.eval(code);
+    addScript(tempFrame.contentWindow.document, code);
     
   })
   
